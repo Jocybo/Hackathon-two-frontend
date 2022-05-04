130c529a391549b2ccb88818988b09dbab883173
jocybo <naveencybosec@gmail.com>
2022-05-04 21:47:17 +0530

    changes

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,76 +2,69 @@ import { useState, useEffect, useContext } from "react";
 import Context from "../context";
 import axios from "axios";
 function Card({ match }) {
+
     const context = useContext(Context);
     const [card, setCard] = useState([]);
 
-    //To  Fetch data
-    let getsearch = async () => {
+
+    let getSearch = async () => {
         if (context.state.length !== 0 || match.path === "/:id") {
             if (match.path === "/:id") {
                 const { data } = await axios.get(
                     `https://webscrapbackend.herokuapp.com/product/${match.params.id}`
                 );
-
                 setCard(data);
             } else {
                 pagination(context.state);
             }
         } else {
-            const { data } = await axios.get(
-                "https://webscrapbackend.herokuapp.com/product/"
-            );
-
+            const { data } = await axios.get("https://webscrapbackend.herokuapp.com/product");
             pagination(data);
         }
     };
 
     useEffect(() => {
-        getsearch();
-    }, []);
+        getSearch();
+    },[]);
 
-    //To Change States --> when params change
     useEffect(() => {
-        getsearch();
+        getSearch();
     }, [match.params.id]);
 
-    //---> Pagination<---//
-
-    let allvalues = [];
-
+    let pageValues = [];
     let pagination = (data) => {
-        allvalues = [...data];
-
+        pageValues = [...data];
         previousPage();
     };
 
     let nextPage = () => {
-        if (allvalues.length === 0) {
-            allvalues = [...context.state];
+        if (pageValues.length === 0) {
+            pageValues = [...context.state];
+
         }
         let temp = [];
 
         let startindex = context.pagenumber;
         let endindex = context.pagenumber + 10;
         for (let i = startindex; i < endindex; i++) {
-            temp.push(allvalues[i]);
+            temp.push(pageValues[i]);
         }
         setCard(temp);
         context.pagenumber = endindex;
-        if (context.pagenumber >= allvalues.length) {
-            context.pagenumber = allvalues.length - 10;
+        if (context.pagenumber >= pageValues.length) {
+            context.pagenumber = pageValues.length - 10;
         }
     };
     let previousPage = () => {
-        if (allvalues.length === 0) {
-            allvalues = [...context.state];
+        if (pageValues.length === 0) {
+            pageValues = [...context.state];
         }
         let temp = [];
 
         let endindex = context.pagenumber;
         let startindex = context.pagenumber - 10;
         for (let i = startindex; i < endindex; i++) {
-            temp.push(allvalues[i]);
+            temp.push(pageValues[i]);
         }
         setCard(temp);
 
@@ -84,47 +77,29 @@ function Card({ match }) {
 
     return (
         <div>
-            <div className="container-fluid  cardgrid">
+            <div className="container-fluid  boxgrid">
                 {card.map((scrapData) => {
                     return (
-                        <div className="card" key={scrapData._id}>
-                            <img
-                                className="card-img-top cardimg"
-                                alt="Mobile"
-                                style={{ width: "100%" }}
-                                src={scrapData.productImage}
-                            ></img>
+                        <div className="box" key={scrapData._id}>
+                            <img className="boximg" alt="Mobile" style={{ width: "100%" }} src={scrapData.productImage}></img>
                             <div className="card-body">
-                                <div className="logo">
-                                    <img
-                                        className="logoimg"
-                                        style={{ width: "100%" }}
-                                        src={scrapData.logo}
-                                        alt="logo"
-                                    />
-                                </div>
                                 <p className="title">{scrapData.productName}</p>
-                                <p className="rating">Rating:{scrapData.productRating}</p>
-                                <p className="finalprice">
-                                    Offer Price:{scrapData.offerPrice}{" "}
-                                    <span className="price">price{scrapData.productPrice}</span>
-                                </p>
+                                <p className="productrating">Rating : {scrapData.productRating}</p>
+                                <p className="offerPrice">Price : {scrapData.offerPrice}<span className="actualPrice"> {scrapData.productPrice}</span></p>
                             </div>
                         </div>
                     );
                 })}
             </div>
             {match.params.id ? (
-                <></>
+                <>
+                </>
             ) : (
-                <div className="pagi">
-                    <button type="button" className="btn butt" onClick={nextPage}>
-                        Next
-                    </button>
-                    <button type="button" className="btn butt" onClick={previousPage}>
+                <div className="pagination mb-4">
+                     <button type="button" className="btn btn-outline-dark fontchange"onClick={previousPage}>
                         Previous
                     </button>
-                    <button type="button" className="btn butt" onClick={nextPage}>
+                    <button type="button" className="btn btn-outline-dark ms-3 fontchange" onClick={nextPage}>
                         Next
                     </button>
                 </div>
